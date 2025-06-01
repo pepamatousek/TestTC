7e849826aa2a8a02ca05041297d6edf5c692f9ae
Pepa <pepacode@seznam.cz>
2025-06-01 14:00:10 +0200

    Přidejte soubory projektu.

---
+++ UnitTestTC/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Xceed.Wpf.Toolkit;
+using TestTC.Controllers;
+
+namespace UnitTestTC
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectResult()
+        {
+            var result = HomeController.Calculator(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}

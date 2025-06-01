3b6251ddeabf3fb828526d481324057d5c9e402d
Pepa <pepacode@seznam.cz>
2025-06-01 18:02:41 +0200

    Failed test added.

--- UnitTestTC/CalculatorTests.cs
+++ UnitTestTC/CalculatorTests.cs
@@ -6,9 +6,16 @@ namespace UnitTestTC
     public class CalculatorTests
     {
         [Fact]
-        public void Add_ShouldReturnCorrectResult()
+        public void Calculator_ShouldReturnCorrectResult()
         {
             var result = HomeController.Calculator(2, 3);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Calculator_ShouldReturnFailedTest()
+        {
+            var result = HomeController.Calculator(3, 3);
             Assert.Equal(5, result);
         }
     }
